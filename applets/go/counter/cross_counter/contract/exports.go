6180ebea348fd66ac96af4aec6bf1882ab1c55d3
Lasaro <lasaro@weilliptic.com>
2025-11-12 11:57:07 -0300

    updating generated code

--- applets/go/counter/cross_counter/contract/exports.go
+++ applets/go/counter/cross_counter/contract/exports.go
@@ -1,21 +1,19 @@
 package contract
 
 import (
-	"github.com/weilliptic-public/wadk/adk/go/weil_go/errors"
-	"github.com/weilliptic-public/wadk/adk/go/weil_go/runtime"
+    "github.com/weilliptic-public/wadk/adk/go/weil_go/errors"
+    "github.com/weilliptic-public/wadk/adk/go/weil_go/runtime"
     "github.com/weilliptic-public/wadk/adk/go/weil_go/types"
 )
 
 
-func coreFetchCounterFrom() (*uint32, errors.WasmHostInterfaceError) {
-    state, _ := runtime.State[CrossCounterContractState]()
-
+func coreFetchCounterFrom() (*uint32, errors.WeilError) {
     type FetchCounterFromArgs struct {
         ContractId string `json:"contract_id"`
     }
-    
-    args, err := runtime.Args[FetchCounterFromArgs]()
-    
+
+    state, args, err := runtime.StateAndArgs[CrossCounterContractState, FetchCounterFromArgs]()
+
     if err != nil {
         return nil, errors.NewMethodArgumentDeserializationError("FetchCounterFrom", err)
     }
@@ -26,51 +24,44 @@ func coreFetchCounterFrom() (*uint32, errors.WasmHostInterfaceError) {
 }
 
 func FetchCounterFrom() {
-    var resp *types.Result[uint32, errors.WasmHostInterfaceError]
+    var resp *types.Result[uint32, errors.WeilError]
 
     result, err := coreFetchCounterFrom()
 
-
     if err != nil {
-		resp = types.NewErrResult[uint32, errors.WasmHostInterfaceError](&err)
+        resp = types.NewErrResult[uint32, errors.WeilError](&err)
     } else {
-		resp = types.NewOkResult[uint32, errors.WasmHostInterfaceError](result)
+        resp = types.NewOkResult[uint32, errors.WeilError](result)
     }
 
     runtime.SetResult(resp)
 }
 
-func coreIncrementCounterOf() errors.WasmHostInterfaceError {
-    state, _ := runtime.State[CrossCounterContractState]()
-
+func coreIncrementCounterOf() *types.Result[runtime.WeilValue[CrossCounterContractState, interface{}], errors.WeilError] {
     type IncrementCounterOfArgs struct {
         ContractId string `json:"contract_id"`
     }
-    
-    args, err := runtime.Args[IncrementCounterOfArgs]()
-    
+
+    state, args, err := runtime.StateAndArgs[CrossCounterContractState, IncrementCounterOfArgs]()
+
     if err != nil {
-        return errors.NewMethodArgumentDeserializationError("IncrementCounterOf", err)
+        var newErr errors.WeilError = errors.NewMethodArgumentDeserializationError("IncrementCounterOf", err)
+        return types.NewErrResult[runtime.WeilValue[CrossCounterContractState, interface{}], errors.WeilError](&newErr)
     }
 
     state.IncrementCounterOf(args.ContractId)
 
-    runtime.SetState(state)
-
-    return nil
+    return types.NewOkResult[runtime.WeilValue[CrossCounterContractState, interface{}], errors.WeilError](runtime.NewWeilValueWithStateAndOkValue[CrossCounterContractState, interface{}](state, nil))
 }
 
 func IncrementCounterOf() {
-    var resp *types.Result[interface{}, errors.WasmHostInterfaceError]
+    result := coreIncrementCounterOf()
 
-    err := coreIncrementCounterOf()
+    runtime.SetStateAndResult(result)
+}
 
+    func Tools() {
+	toolDefs := `[]`
 
-    if err != nil {
-		resp = types.NewErrResult[interface{}, errors.WasmHostInterfaceError](&err)
-    } else {
-		resp = types.NewOkResult[interface{}, errors.WasmHostInterfaceError](nil)
-    }
-
-    runtime.SetResult(resp)
+	runtime.SetResult(types.NewOkResult[string, errors.WeilError](&toolDefs))
 }
--- applets/go/counter/cross_counter/main.go
+++ applets/go/counter/cross_counter/main.go
@@ -19,40 +19,45 @@ func Free(ptr uintptr, len uint) {
 	runtime.Deallocate(ptr, len)
 }
 
+//export tools
+func Tools() {
+	contract.Tools()
+}
+
 //export init
 func Init() {
-	var resp *types.Result[interface{}, errors.WasmHostInterfaceError]
+    var resp *types.Result[runtime.WeilValue[contract.CrossCounterContractState, interface{}], errors.WeilError]
 	state, err := contract.NewCrossCounterContractState()
 
-	if err != nil {
-		var newErr errors.WasmHostInterfaceError = errors.NewFunctionReturnedWithError("NewCrossCounterContractState", err)
-		resp = types.NewErrResult[interface{}](&newErr)
-	} else {
-		resp = types.NewOkResult[interface{}, errors.WasmHostInterfaceError](nil)
-	}
+    if err != nil {
+		var newErr errors.WeilError = errors.NewFunctionReturnedWithError("NewCrossCounterContractState", err)
+		resp = types.NewErrResult[runtime.WeilValue[contract.CrossCounterContractState, interface{}], errors.WeilError](&newErr)
+    } else {
+		resp = types.NewOkResult[runtime.WeilValue[contract.CrossCounterContractState, interface{}], errors.WeilError](runtime.NewWeilValueWithStateAndOkValue[contract.CrossCounterContractState, interface{}](state, nil))
+    }
 
-	runtime.SetState(state)
-	runtime.SetResult(resp)
+	runtime.SetStateAndResult(resp)
 }
 
 //export fetch_counter_from
 func FetchCounterFrom() {
-	contract.FetchCounterFrom()
+    contract.FetchCounterFrom()
 }
 
 //export increment_counter_of
 func IncrementCounterOf() {
-	contract.IncrementCounterOf()
+    contract.IncrementCounterOf()
 }
 
 //export method_kind_data
 func MethodKindData() {
-	methodKindMapping := jsonmap.New()
+    methodKindMapping := jsonmap.New()
 
-	methodKindMapping.Set("fetch_counter_from", "query")
-	methodKindMapping.Set("increment_counter_of", "mutate")
+    methodKindMapping.Set("fetch_counter_from", "query")
+    methodKindMapping.Set("increment_counter_of", "mutate")
 
-	resp := types.NewOkResult[jsonmap.Map, errors.WasmHostInterfaceError](methodKindMapping)
+
+    resp := types.NewOkResult[jsonmap.Map, errors.WeilError](methodKindMapping)
 	runtime.SetResult(resp)
 }
 

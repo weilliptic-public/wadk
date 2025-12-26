f87e0f8fc26e1d43e2a6c095acae8b5b9fee379d
coderSomya <isomya13@gmail.com>
2025-12-26 22:50:41 +0530

    chore: remove step agent for now

--- applets/rust/step_agent/README.md
+++
@@ -1,5 +0,0 @@
-## To deploy
-
-```
-deploy -e -f <path_to_wasm> -p <path_to_widl> -i '{"mcp_contract_addresses":[]}
-```
--- applets/rust/step_agent/do_all.sh
+++
@@ -1,3 +0,0 @@
-cargo b --release --target wasm32-unknown-unknown
-
-echo '--widl-file'  `pwd`/*.widl '--file-path' `pwd`/target/wasm32-unknown-unknown/release/*.wasm
